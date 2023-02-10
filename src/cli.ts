2ca04bbe7cd4fd9cd71a820a61892a80fbce08ad
Jon Oropeza <joropeza@gmail.com>
2023-02-10 10:31:43 -0800

    update sqllite to maj version 8

--- src/cli.ts
+++ src/cli.ts
@@ -1,31 +1,13 @@
 #!/usr/bin/env node
-import { createDatabase } from './main'
+
+import { createDatabase } from './main';
 
 require('dotenv').config();
 
 const mapFunction = (match: any) => ({
   date: match.Date,
-  year: parseInt(match.Date.substring(match.Date.length - 4), 10),
-  quality: match.Quality,
-  tags: match.Tags, // TODO put this into an array
-  summary: match.Summary,
-  workout: match.Workout,
-  meditation: match.Meditation,
-  exuberance: match.Exuberance,
-  work: match.Work,
-  conversation: match.Conversation,
-  annoyance: match.Annoyance,
-  insomnia: match.Insomnia,
-  health: match.Health,
-  weight: match.Weight,
-  wine: match.Wine,
-  opportunity: match.Opportunity,
-  writing: match.Writing,
-  hacking: match.Hacking,
-  reading: match.Reading,
-  participation: match.Participation,
-  travel: match.Travel,
-})
+  things: match.Things,
+});
 
 const config = {
   apiKey: process.env.SHEETS_API_KEY || '',
